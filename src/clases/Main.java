96cdd2261671a93cc7124132b591d42ed7a2969a
Vann06 <127684976+Vann06@users.noreply.github.com>
2024-02-21 22:03:08 -0600

    Inicio

---
+++ src/clases/Main.java
@@ -0,0 +1,12 @@
+package clases;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("voy a exploadadadtar");
+	}
+
+}
