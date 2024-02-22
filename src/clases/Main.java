00bb02e40e3d2a78a23e0742817c829a4a8c1962
Ricgo01 <128996354+Ricgo01@users.noreply.github.com>
2024-02-21 22:40:18 -0600

    Creacion de clases
    Se crearon las clases del programa, asi como la implementacion del factory

--- src/clases/Main.java
+++ src/clases/Main.java
@@ -1,12 +1,44 @@
 package clases;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        
+        Stack<?> stack = null;
+        List<?> lista = null;
+        while (true) {
+        	
+            System.out.println("Bienvenido a la calculadora de postfix  :3333");
+            System.out.println("Por favor seleccione una opcion");
+            System.out.println("1. Para el uso de una lista simple");
+            System.out.println("2. Para lista doblemente enlazada");
+            System.out.println("3. Para el uso de un vector");
+            System.out.println("4. Para el uso de un ArrayList");
+            System.out.println("5. Para salir");
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+            Scanner sc = new Scanner(System.in);
+            int opcion = sc.nextInt();
 
-		
-		System.out.println("voy a exploadadadtar");
-	}
+            if (opcion == 1 || opcion == 2) {
+                lista = DataStructureFactory.getList(opcion);
+                Calculadora.leerYCalcular("datos.txt");
+            }
+            else if (opcion == 3 || opcion == 4) {
+                stack = DataStructureFactory.getStack(opcion);
+                Calculadora.leerYCalcular("datos.txt");
+            }
+            else if (opcion == 5) {
+                System.out.println("Gracias por usar la calculadora");
+                break;
+            }
+            else {
+                System.out.println("Opcion no valida");
+                continue;
+                
+               
+            }
+        }
 
+    }
 }
---
+++ src/clases/VectorStack.java
@@ -0,0 +1,32 @@
+package clases;
+
+import java.util.Vector;
+
+
+public class VectorStack <E> implements Stack<E>{
+
+    @Override
+    public void push(E item) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'push'");
+    }
+
+    @Override
+    public E pop() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pop'");
+    }
+
+    @Override
+    public E peek() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'peek'");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+    }
+    
+}
